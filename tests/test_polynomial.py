950ed5ebfc84f9ce43ffd2edaa6666aebbd8e241
Esdras Battosti <esdrasbattostisilva@gmail.com>
2025-07-05 19:49:30 -0300

    Add tests for polynomial creation exceptions

--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -24,3 +24,29 @@ def test_polynomial_string_representation(input_data, expected_string):
     poly = Polynomial(*input_data)
 
     assert str(poly) == expected_string
+
+
+@pytest.mark.parametrize(
+    "input_data,expected_exception",
+    [
+        # string input as exponent
+        (("polyany", [[1, 2, 3]]), TypeError),
+        # string input as coefficient
+        (([[1, 2], [0, 1], [0, 0]], "polyany"), TypeError),
+        # none input
+        (([[1, 2], [0, 1], [0, 0]], None), TypeError),
+        # float exponents (non safe-convertible to int)
+        (([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0]], [[-1, 2, 33]]), TypeError),
+        # exponents without 2 dimensions
+        (([1, 2, 3], [10, 11, 12]), ValueError),
+        # coefficients without 1 dimension
+        (([[0, 0], [1, 0], [0, 1]], [[1, 2], [3, 4]]), ValueError),
+        # different number of exponents and coefficients
+        (([[1, 2], [0, 0], [1, 1], [2, 1]], [10, 11, 12]), ValueError),
+        # nonlinear polynomial (negative exponents)
+        (([[0, 0], [-1, 0], [0, -1]], [-1, 1, -1]), ValueError),
+    ],
+)
+def test_polynomial_creation_exceptions(input_data, expected_exception):
+    with pytest.raises(expected_exception):
+        Polynomial(*input_data)
