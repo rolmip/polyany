11ff7f306d2218f5ec527c6ae61de8ea7e703dac
Esdras Battosti <esdrasbattostisilva@gmail.com>
2025-07-22 17:59:24 -0300

    feat: add basic comparison methods

--- src/polyany/polynomial.py
+++ src/polyany/polynomial.py
@@ -335,3 +335,15 @@ class Polynomial:
         return self.coefficients @ np.prod(
             np.power(converted_point, self.exponents), axis=1
         )
+
+    def __lt__(self, other: object) -> bool:
+        return NotImplemented
+
+    def __le__(self, other: object) -> bool:
+        return NotImplemented
+
+    def __gt__(self, other: object) -> bool:
+        return NotImplemented
+
+    def __ge__(self, other: object) -> bool:
+        return NotImplemented
