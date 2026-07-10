43383a4cdd27811ffcc627b154d3ed6a05eb5541
Esdras Battosti <esdrasbattostisilva@gmail.com>
2026-07-10 17:26:06 -0300

    fix(add): using ravel to fix numpy ufunc error

--- src/polyany/polynomial.py
+++ src/polyany/polynomial.py
@@ -554,7 +554,7 @@ class Polynomial:
 
         exponents, indices = np.unique(stacked_exponents, axis=0, return_inverse=True)
         coefficients = np.zeros(len(exponents))
-        np.add.at(coefficients, indices, stacked_coefficients)
+        np.add.at(coefficients, indices.ravel(), stacked_coefficients)
 
         return self.__class__(exponents, coefficients)
 
