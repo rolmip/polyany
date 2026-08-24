11626107ac937ab4988b33a259605b44775098b9
Esdras Battosti <esdrasbattostisilva@gmail.com>
2026-08-24 14:51:51 -0300

    test: add test cases for empty coefficients

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -81,6 +81,8 @@ def test_polynomial_latex_representation(
         (([[0, 0], [1, 0], [0, 1]], [[1, 2], [3, 4]]), ValueError),
         # coefficients without 1 dimension
         (([[0, 0], [1, 0], [0, 1]], [[1, 2, 3]]), ValueError),
+        # empty coefficients
+        (([[0]], np.array([], dtype=np.float64)), ValueError),
         # scalar coefficient
         (([[0, 0]], 0), ValueError),
         # non unique exponents
@@ -215,6 +217,10 @@ def test_matrix_polynomial_string_representation(input_data, expected_string):
         (([[0, 0], [1, 0], [0, 1]], [[1, 2], [3, 4]]), ValueError),
         # matrix coefficients without 3 dimensions
         (([[0, 0], [1, 0], [0, 1]], [[1, 2, 3]]), ValueError),
+        # empty coefficient
+        (([[0]], np.zeros((1, 0, 1))), ValueError),
+        # empty coefficient
+        (([[0]], np.array([], dtype=np.float64).reshape(1, 0, 1)), ValueError),
         # non safe-convertible coefficient to float
         (([[0, 0]], [[["1"]]]), TypeError),
     ],
