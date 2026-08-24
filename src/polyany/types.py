b7c4728f16fe25619496f1f8ff8611665b43d515
Esdras Battosti <esdrasbattostisilva@gmail.com>
2026-08-24 09:11:16 -0300

    chore(types): add matrixalgebraic type

--- src/polyany/types.py
+++ src/polyany/types.py
@@ -1,10 +1,14 @@
 from typing import TypeAlias
 
 import numpy as np
+from numpy.typing import ArrayLike
 
+from .matrix import MatrixPolynomial
 from .polynomial import Polynomial
 
 Scalar: TypeAlias = int | float | np.integer | np.floating
 """A numeric scalar that can be a builtin numeric type or a NumPy scalar."""
 ScalarAlgebraic: TypeAlias = Scalar | Polynomial
 """An algebraic element that can be a scalar or a scalar Polynomial."""
+MatrixAlgebraic: TypeAlias = ArrayLike | MatrixPolynomial
+"""An algebraic element that can be a matrix or a matrix Polynomial."""
