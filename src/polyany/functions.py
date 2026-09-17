766c0ab98737ba5f7df3c99b1fc006266e59b8d0
Esdras Battosti <esdrasbattostisilva@gmail.com>
2026-09-17 14:51:56 -0300

    feat: add block method

--- src/polyany/functions.py
+++ src/polyany/functions.py
@@ -119,3 +119,62 @@ def concatenate(
     )
 
     return MatrixPolynomial(unique_exponents, concatenated_coefficients)
+
+
+def block(polynomials: Sequence[Sequence[MatrixPolynomial]]) -> MatrixPolynomial:
+    """Create a matrix polynomial block
+
+    A polynomial block is the structure formed by concatenating polynomials both
+    vertically and horizontally.
+
+    Parameters
+    ----------
+    polynomials : Sequence[Sequence[MatrixPolynomial]]
+        A nested sequence (lists or tuples) of matrix polynomials to assemble.
+
+    Returns
+    -------
+    block_polynomial : MatrixPolynomial
+        A new matrix polynomial with assembled coefficients.
+
+    Raises
+    ------
+    TypeError
+        - If any of the inner elements are not a sequence (list or tuple).
+        - If any of the inner sequence elements are not a MatrixPolynomial.
+
+    Notes
+    -----
+    Similar to NumPy behavior, the inner sequence is concatenated horizontally and
+    then the resulting polynomials are concatenated vertically.
+
+    Examples
+    --------
+    >>> mpoly1 = MatrixPolynomial([[1], [2]], [np.eye(2), np.tri(2)])
+    >>> mpoly2 = MatrixPolynomial([[1]], [np.ones((2,2))])
+    >>> mpoly3 = MatrixPolynomial([[1]], [3*np.ones((3,2))])
+    >>> mpoly4 = MatrixPolynomial([[2]], [np.arange(6).reshape(3, 2)])
+    >>> block([[mpoly1, mpoly2], [mpoly3, mpoly4]])
+    [[1. 0. 1. 1.]        [[1. 0. 0. 0.]
+     [0. 1. 1. 1.]         [1. 1. 0. 0.]
+     [3. 3. 0. 0.]         [0. 0. 0. 1.]
+     [3. 3. 0. 0.]         [0. 0. 2. 3.]
+     [3. 3. 0. 0.]]*x_1 +  [0. 0. 4. 5.]]*x_1^2
+    """
+    if not all(
+        isinstance(inner_element, (list, tuple)) for inner_element in polynomials
+    ):
+        msg = "The inner elements must be a list/tuple of MatrixPolynomial objects."
+        raise TypeError(msg)
+
+    if not all(
+        isinstance(element, MatrixPolynomial)
+        for inner_list in polynomials
+        for element in inner_list
+    ):
+        msg = "All elements to be assembled must be MatrixPolynomial objects."
+        raise TypeError(msg)
+
+    concatenated_rows = [concatenate(row, axis=1) for row in polynomials]
+
+    return concatenate(concatenated_rows)
